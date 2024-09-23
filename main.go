c2a38e2051142b3624c71a022380c0989aa2f50f
ksp20 <saipraneethreddy2001@gmail.com>
2024-09-23 12:43:12 -0400

    node_addr fix

--- main.go
+++ main.go
@@ -30,8 +30,8 @@ import (
 )
 
 var (
-	server          = "s5"
-	server_addr     = 61005
+	node_id         = "SBU_ID" // give your SBU ID
+	port            = 61005
 	relay_node_addr = "/ip4/130.245.173.222/tcp/4000/p2p/12D3KooWSMDFN5DeFADosuV7UEwjHWQv1ioLEUqgzjGjxYQzxFX6"
 	//"/ip4/130.245.173.221/tcp/4001/p2p/12D3KooWDpJ7As7BWAwRMfu1VU2WCqNjvq387JEYKDBj4kx6nXTN"
 	bootstrap_node_addr = "/ip4/130.245.173.222/tcp/61000/p2p/12D3KooWPcfGdBCrdxX9nqGAdPAdkPMqfKEDjbZWGA4UFBJuY4rP"
@@ -39,8 +39,7 @@ var (
 )
 
 func generatePrivateKeyFromSeed(seed []byte) (crypto.PrivKey, error) {
-	// Generate deterministic key material
-	hash := sha256.Sum256(seed)
+	hash := sha256.Sum256(seed) // Generate deterministic key material
 	// Create an Ed25519 private key from the hash
 	privKey, _, err := crypto.GenerateEd25519Key(
 		bytes.NewReader(hash[:]),
@@ -63,8 +62,8 @@ func isPortAvailable(port int) bool {
 
 func createNode() (host.Host, *dht.IpfsDHT, error) {
 	ctx := context.Background()
-	node_addr := server_addr
-	seed := []byte(server)
+	node_addr := port
+	seed := []byte(node_id)
 	for !isPortAvailable(node_addr) {
 		node_addr++
 	}
@@ -96,8 +95,7 @@ func createNode() (host.Host, *dht.IpfsDHT, error) {
 		libp2p.EnableNATService(),
 		libp2p.EnableAutoRelayWithStaticRelays([]peer.AddrInfo{*relayInfo}),
 		libp2p.EnableRelayService(),
-		// libp2p.EnableAutoRelay(),
-		// libp2p.StaticRelays(staticRelays),
+		libp2p.EnableHolePunching(),
 	)
 
 	if err != nil {
@@ -115,8 +113,8 @@ func createNode() (host.Host, *dht.IpfsDHT, error) {
 	namespacedValidator := record.NamespacedValidator{
 		"myapp": &CustomValidator{}, // Add a custom validator for the "myapp" namespace
 	}
-	// Configure the DHT to use the custom validator
-	dhtRouting.Validator = namespacedValidator
+
+	dhtRouting.Validator = namespacedValidator // Configure the DHT to use the custom validator
 
 	err = dhtRouting.Bootstrap(ctx)
 	if err != nil {
@@ -157,84 +155,8 @@ func connectToPeer(node host.Host, peerAddr string) {
 	fmt.Println("Connected to:", info.ID)
 }
 
-// func findPeer(dht *dht.IpfsDHT, peerID peer.ID) {
-// 	ctx := context.Background()
-// 	_, err := dht.FindPeer(ctx, peerID)
-// 	if err != nil {
-// 		log.Printf("Failed to find peer: %s", err)
-// 		return
-// 	}
-// 	fmt.Println("Found peer:", peerID)
-// }
-
-// func exchangePeers(node host.Host, newPeer peer.ID) {
-// 	knownPeers := node.Peerstore().Peers()
-// 	var peerInfos []string
-// 	for _, peer := range knownPeers {
-// 		if peer != newPeer && peer != node.ID() {
-// 			addrs := node.Peerstore().Addrs(peer)
-// 			for _, addr := range addrs {
-// 				peerAddr := addr.String() + "/p2p/" + peer.String()
-// 				peerInfos = append(peerInfos, peerAddr)
-// 			}
-// 		}
-// 	}
-// 	// Send peer info to the new peer
-// 	s, err := node.NewStream(context.Background(), newPeer, "/peer-exchange/1.0.0")
-// 	if err != nil {
-// 		log.Printf("Failed to open stream to %s: %s", newPeer, err)
-// 		return
-// 	}
-// 	defer s.Close()
-// 	for _, info := range peerInfos {
-// 		_, err := s.Write([]byte(info + "\n"))
-// 		if err != nil {
-// 			log.Printf("Failed to send peer info to %s: %s", newPeer, err)
-// 			return
-// 		}
-// 	}
-// 	fmt.Printf("Shared %d peers with %s\n", len(peerInfos), newPeer.String())
-// }
-
-// func handlePeerExchange(node host.Host) {
-// 	node.SetStreamHandler("/peer-exchange/1.0.0", func(s network.Stream) {
-// 		defer s.Close()
-// 		buf := bufio.NewReader(s)
-// 		for {
-// 			peerAddr, err := buf.ReadString('\n')
-// 			if err != nil {
-// 				if err != io.EOF {
-// 					log.Printf("Error reading from stream: %s", err)
-// 				}
-// 				return
-// 			}
-// 			connectToPeerUsingRelay(node, peerAddr)
-// 			// peerAddr = strings.TrimSpace(peerAddr)
-// 			// // Parse the peer address
-// 			// addr, err := multiaddr.NewMultiaddr(peerAddr)
-// 			// if err != nil {
-// 			// 	log.Printf("Invalid peer address received: %s", err)
-// 			// 	continue
-// 			// }
-// 			// // Extract the peer ID from the address
-// 			// info, err := peer.AddrInfoFromP2pAddr(addr)
-// 			// if err != nil {
-// 			// 	log.Printf("Failed to extract peer info: %s", err)
-// 			// 	continue
-// 			// }
-// 			// // Add the peer to the peerstore
-// 			// node.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
-// 			// // Optionally, try to connect to the new peer
-// 			// if err := node.Connect(context.Background(), *info); err != nil {
-// 			// 	log.Printf("Failed to connect to peer %s: %s", info.ID, err)
-// 			// } else {
-// 			// 	fmt.Printf("Connected to new peer: %s\n", info.ID)
-// 			// }
-// 		}
-// 	})
-// }
-
-func handlePeerExchange1(node host.Host) {
+func handlePeerExchange(node host.Host) {
+	relayInfo, _ := peer.AddrInfoFromString(relay_node_addr)
 	node.SetStreamHandler("/peer-exchange/1.0.0", func(s network.Stream) {
 		defer s.Close()
 
@@ -256,58 +178,15 @@ func handlePeerExchange1(node host.Host) {
 				fmt.Println("Peer:")
 				if peerMap, ok := peer.(map[string]interface{}); ok {
 					if peerID, ok := peerMap["peer_id"].(string); ok {
-						if string(peerID) != "12D3KooWSMDFN5DeFADosuV7UEwjHWQv1ioLEUqgzjGjxYQzxFX6" {
+						if string(peerID) != string(relayInfo.ID) {
 							connectToPeerUsingRelay(node, peerID)
 						}
 					}
-					// if peerAddr, ok := peerMap["peer_addr"].(string); ok {
-					// 	connectToPeerUsingRelay(node, peerAddr)
-					// }
 				}
 			}
 		}
 	})
 }
-
-// func announceToDHT(ctx context.Context, dht *dht.IpfsDHT, node host.Host) {
-// 	backoff := time.Second
-// 	maxBackoff := time.Hour
-// 	for {
-// 		err := dht.Provide(ctx, peer.ToCid(node.ID()), true)
-// 		if err != nil {
-// 			log.Printf("Error providing to DHT: %s", err)
-// 			backoff = min(backoff*2, maxBackoff)
-// 		} else {
-// 			backoff = time.Hour // Reset to 1 hour on success
-// 		}
-// 		time.Sleep(backoff)
-// 	}
-// }
-
-// func discoverPeers(ctx context.Context, dht *dht.IpfsDHT, node host.Host) {
-// 	// Create a unique CID for discovery
-// 	discoveryCID := peer.ToCid(node.ID())
-// 	for {
-// 		fmt.Println("Searching for peers...")
-// 		peerChan := dht.FindProvidersAsync(ctx, discoveryCID, 10)
-// 		for p := range peerChan {
-// 			if p.ID != node.ID() {
-// 				err := node.Connect(ctx, p)
-// 				if err != nil {
-// 					log.Printf("Error connecting to discovered peer %s: %s", p.ID, err)
-// 				} else {
-// 					fmt.Printf("Connected to discovered peer: %s\n", p.ID)
-// 				}
-// 			}
-// 		}
-// 		// Provide our own CID to the network
-// 		err := dht.Provide(ctx, discoveryCID, true)
-// 		if err != nil {
-// 			log.Printf("Error providing to DHT: %s", err)
-// 		}
-// 		time.Sleep(5 * time.Minute)
-// 	}
-// }
 
 func connectToPeerUsingRelay(node host.Host, peerAddrStr string) {
 	ctx := globalCtx
@@ -330,22 +209,7 @@ func connectToPeerUsingRelay(node host.Host, peerAddrStr string) {
 	}
 
 	fmt.Printf("Connected to peer via relay: %s\n", peerAddrStr)
-
 }
-
-// Not useful can be removed -->
-// func monitorPeers(node host.Host) {
-// 	knownPeers := make(map[peer.ID]bool)
-// 	for {
-// 		time.Sleep(5 * time.Second)
-// 		for _, peerID := range node.Network().Peers() {
-// 			if !knownPeers[peerID] {
-// 				fmt.Printf("New peer in peerstore: %s\n", peerID.String())
-// 				knownPeers[peerID] = true
-// 			}
-// 		}
-// 	}
-// }
 
 func main() {
 	node, dht, err := createNode()
@@ -361,27 +225,9 @@ func main() {
 	fmt.Println("Node Peer ID:", node.ID())
 
 	connectToPeer(node, relay_node_addr)     // connect to relay node
+	makeReservation(node)                    // make reservation on realy node
 	connectToPeer(node, bootstrap_node_addr) // connect to bootstrap node
-
-	handlePeerExchange1(node)
-
-	// go announceToDHT(ctx, dht, node)
-	// go discoverPeers(ctx, dht, node)
-	// go monitorPeers(node)
-	// go findProviders(dht, "exampleKey")
-
-	if len(os.Args) > 1 {
-		// peerAddr := os.Args[1]
-		// connectToPeer(node, peerAddr)
-
-		// peerInfo, err := peer.AddrInfoFromString(peerAddr)
-		// if err != nil {
-		// 	log.Printf("Failed to extract peer ID: %s", err)
-		// } else {
-		// 	findPeer(dht, peerInfo.ID)
-		// }
-	}
-	makeReservation(node)
+	go handlePeerExchange(node)
 	go handleInput(ctx, dht)
 
 	defer node.Close()
@@ -469,22 +315,6 @@ func handleInput(ctx context.Context, dht *dht.IpfsDHT) {
 			}
 			key := args[1]
 			provideKey(ctx, dht, key)
-
-			// data := []byte(key)
-			// hash := sha256.Sum256(data)
-			// mh, err := multihash.EncodeName(hash[:], "sha2-256")
-			// if err != nil {
-			// 	fmt.Printf("Error encoding multihash: %v\n", err)
-			// 	continue
-			// }
-			// c := cid.NewCidV1(cid.Raw, mh)
-			// err = dht.Provide(ctx, c, true)
-			// if err != nil {
-			// 	fmt.Printf("Failed to start providing key: %v\n", err)
-			// 	continue
-			// }
-			// fmt.Println("Started providing key")
-
 		default:
 			fmt.Println("Expected GET, GET_PROVIDERS, PUT or PUT_PROVIDER")
 		}
@@ -505,23 +335,15 @@ func provideKey(ctx context.Context, dht *dht.IpfsDHT, key string) error {
 	if err != nil {
 		return fmt.Errorf("failed to start providing key: %v", err)
 	}
-
-	fmt.Println("Started providing key")
 	return nil
 }
 
 func makeReservation(node host.Host) {
 	ctx := globalCtx
-	relayAddr, err := multiaddr.NewMultiaddr(relay_node_addr)
-	// relayMultiaddr := relayAddr.Encapsulate(multiaddr.StringCast("/p2p-circuit"))
+	relayInfo, err := peer.AddrInfoFromString(relay_node_addr)
 	if err != nil {
-		log.Fatalf("Failed to create relay multiaddr: %v", err)
+		log.Fatalf("Failed to create addrInfo from string representation of relay multiaddr: %v", err)
 	}
-	relayInfo, err := peer.AddrInfoFromP2pAddr(relayAddr)
-	if err != nil {
-		log.Fatalf("Failed to create AddrInfo from relay multiaddr: %v", err)
-	}
-	log.Println("reserve ", relayInfo)
 	_, err = client.Reserve(ctx, node, *relayInfo)
 	if err != nil {
 		log.Fatalf("Failed to make reservation on relay: %v", err)
